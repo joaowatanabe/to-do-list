a6c16a950443ea41034cd509596d85d9c63bbf20
wabemusic <jvwatanabe@gmail.com>
2024-08-06 17:09:06 -0300

    arquivos criados

---
+++ src/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    senha TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    tarefa TEXT NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    data DATE NOT NULL DEFAULT NOW(),
+    endereco TEXT NOT NULL,
+    usuario_id INTEGER NOT NULL REFERENCES usuarios(id)
+);
