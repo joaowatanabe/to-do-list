db101582cdfb2e32a259128c8ee813915a19e47c
wabemusic <jvwatanabe@gmail.com>
2024-08-06 21:02:32 -0300

    rotas prontas

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,7 +2,15 @@ const express = require("express");
 const { cadastrarUsuario } = require("../controller/usuarios/cadastroUsuario");
 const { login } = require("../controller/usuarios/loginUsuario");
 const { cadastrarTarefa } = require("../controller/todos/registerTask");
-
+const verificaLogin = require('../middleware/verificarLogin')
 const router = express.Router();
+
+router.post('/cadastro', cadastrarUsuario)
+router.post('/login', login)
+
+router.use(verificaLogin)
+
+router.post('/cadastrarTarefa', cadastrarTarefa)
+
 
 module.exports = router;
